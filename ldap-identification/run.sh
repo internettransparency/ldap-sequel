a99106c2fc4098534b2996d151e7c48271b119fc
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2024-07-02 14:22:03 +0200

    update dev

--- ldap-identification/run.sh
+++ ldap-identification/run.sh
@@ -4,5 +4,16 @@ INPUT_CSV=$1
 OUTPUT_CSV=$2
 SHARED_DIR="shared_dir"
 
-podman run --rm -v "$(pwd)":/${SHARED_DIR} ldap-identification -input-csv="${SHARED_DIR}/${INPUT_CSV}" -output="${SHARED_DIR}/${OUTPUT_CSV}" -v 2 -log-file="${SHARED_DIR}/output.log"
+cp "${INPUT_CSV}" . 2>/dev/null
+output_filename=$(basename "${OUTPUT_CSV}")
+input_filename=$(basename "${INPUT_CSV}")
+
+podman run --rm -v "$(pwd)":/${SHARED_DIR} ldap-identification -input-csv="${SHARED_DIR}/${input_filename}" -output="${SHARED_DIR}/${output_filename}" -v 2 -log-file="${SHARED_DIR}/output.log"
+
+a=$(realpath "${input_filename}")
+b=$(realpath "${INPUT_CSV}")
+if [[ "${a}" != "${b}" ]]; then
+    rm "${input_filename}"
+fi
+mv "${output_filename}" "${OUTPUT_CSV}" 2>/dev/null
 
